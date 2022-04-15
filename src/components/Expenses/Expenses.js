93908f2656f1de3d40445640d78ff051414d5dbb
Divyansh <uic.16mca8177@gmail.com>
2022-04-15 03:38:34 -1100

    first course

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,13 +3,12 @@ import './Expenses.css'
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
-
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = selectedYear => {
-    console.log('Expenses.js');
     setFilteredYear(selectedYear);
   }
 
@@ -24,6 +23,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        
+        <ExpensesChart expenses={filteredExpenses}/>
       <ExpensesList expenses={filteredExpenses}/>
       </Card>
     </div>
